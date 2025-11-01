45422724560ffb94bb840729a3454fb0a2cdf4e0
Khoa Le <lnanhkhoa303@gmail.com>
2025-11-01 18:51:14 +0700

    refactor: reinit apps/web

---
+++ apps/web/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {
+  //  Inter, Lusitana,
+  Geist_Mono
+} from 'next/font/google'
+
+// export const inter = Inter({ subsets: ['latin'] })
+// export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+export const geistMono = Geist_Mono({ subsets: ['latin'], weight: ['400', '700'] })
